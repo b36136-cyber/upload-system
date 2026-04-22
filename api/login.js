4fbd413c28f9b7f778ac6754bff0524ee09c490a
b36136-cyber <b.36136@gmail.com>
2026-04-22 14:23:34 +0400

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,10 +1,12 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+
   const USERS = {
     ahmed: { password: "1234", folder: "ahmed" },
     ali: { password: "1234", folder: "ali" }
   };
 
-  const { username, password } = req.body;
+  const { username, password } = body;
 
   const user = USERS[username];
 
